89fb6919baad42bae4efe7b97f8d2a3ae9e679aa
Arif Hidayat <arifhidayat1010@gmail.com>
2020-08-08 01:10:12 +0700

    [UPDATE] add template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AboutComponent } from './components/about/about.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { IntroductionComponent } from './components/introduction/introduction.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { TimelineComponent } from './components/timeline/timeline.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    BlogComponent,
+    IntroductionComponent,
+    ProjectsComponent,
+    SidebarComponent,
+    TimelineComponent
   ],
   imports: [
     BrowserModule,
